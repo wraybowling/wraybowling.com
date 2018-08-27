d73f5bbd03e9b34dff92c3c009925b8192a2d678
Wray Bowling <fabmud@gmail.com>
2018-08-27 14:15:18 -0400

    fixme: dest dist

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,8 +3,8 @@
 
 module.exports = {
   title: 'Wray Bowling',
-  description: "The web site or Wray Bowling",
-  dest: 'dist', //destination
+  description: "The web site of Wray Bowling",
+  dest: 'dist', // build destination confusingly called distribution
 	themeConfig: {
 		repo: 'wraybowling/wraybowling.com',
 		//repoLabel: 'Improve this site',
