1c51acd5b79ba70cf4aabb67c55254fc545ae86b
Wray Bowling <1120619+wraybowling@users.noreply.github.com>
2023-12-05 17:32:38 -0500

    upgrades to nuxt 3

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,21 @@
 <template>
   <div class="container">
-# Wray Bowling
+    # Wray Bowling ## Essays 1. [A 30 Card Sort](/essays/30-card-sort/) 2. [The
+    Ultimate Content Filter](/essays/ultimate-content-filter) 3. [A 3-Way Range
+    Input](/essays/triangular-range-input/) ## Well, here we are My name is
+    Wray. The W is silent. I've been doing web development for
+    <since time="1 Aug 2000 2:30:00 GMT-0400 (EDT)" />. This is my new web site
+    where I divulge deeply on my methods, processes, projects, and the people
+    who have helped me make it all happen. Writing and development is done in
+    the open using Github, Netlify, Vuepress, & Markdown. [You can contribute by
+    proof-reading future
+    content.](https://github.com/wraybowling/wraybowling.com/pulls) But wait!
+    There's more! I'm looking for new work; [my CV is a web page](/cv.md).
 
-## Essays
-1. [A 30 Card Sort](/essays/30-card-sort/)
-2. [The Ultimate Content Filter](/essays/ultimate-content-filter)
-3. [A 3-Way Range Input](/essays/triangular-range-input/)
-
-
-## Well, here we are
-My name is Wray. The W is silent. I've been doing web development for <since time="1 Aug 2000 2:30:00 GMT-0400 (EDT)"/>. This is my new web site where I divulge deeply on my methods, processes, projects, and the people who have helped me make it all happen.
-
-Writing and development is done in the open using Github, Netlify, Vuepress, & Markdown. [You can contribute by proof-reading future content.](https://github.com/wraybowling/wraybowling.com/pulls)
-
-But wait! There's more!
-I'm looking for new work; [my CV is a web page](/cv.md).
-
-<h2>Users</h2>
+    <h2>Users</h2>
     <ul class="users">
       <li v-for="user in users" :key="user.id">
-        <NuxtLink :to="'/projects/'+user.id">
+        <NuxtLink :to="'/projects/' + user.id">
           {{ user.name }}
         </NuxtLink>
       </li>
@@ -28,14 +24,13 @@ I'm looking for new work; [my CV is a web page](/cv.md).
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
   async asyncData() {
-    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
-    return { users: data }
-  }
-}
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await JSON.parse(response.text());
+    return { users: data };
+  },
+};
 </script>
 
 <style scoped>
@@ -60,5 +55,3 @@ export default {
   color: orange;
 }
 </style>
-
-
--- readme.md
+++ readme.md
@@ -1,10 +1,46 @@
-[![Linux Build Status](https://travis-ci.org/wraybowling/wraybowling.com.svg?branch=master)](https://travis-ci.org/wraybowling/wraybowling.com)
+# I made this for me
 
-## I made this for me
 I like making things that are interactive, respond instantaneously, and communicate ideas elegantly. I’m big on small files that do a lot, banishing unnecessary stateful UX, documentation as I go, and recyclable object-oriented structures. When I’m not programming for work, I do it for fun, and when I’m not designing things for work I do it for fun. I’m seeking a position that is looking for someone who has a passion for bridging the gap or even managing it.
 
-## I made this for you, too
+# I made this for you, too
+
 wraybowling.com and its contents are being built completely in the open on Github. Comments and pull requests are welcome. If you're interested in building a site of your own the same way, there are a number of blog posts on that very subject. Similarly, this site is sort of a meta-site that explains web technologies by embodying those technologies. The code is written to be be as human friendly as possible.
 
 Thanks for stopping by,
 Wray Bowling
+
+# Rebuilt With Nuxt 3
+
+Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
+
+## Setup
+
+Make sure to install the dependencies:
+
+```bash
+pnpm install
+```
+
+## Development Server
+
+Start the development server on `http://localhost:3000`:
+
+```bash
+pnpm run dev
+```
+
+## Production
+
+Build the application for production:
+
+```bash
+pnpm run build
+```
+
+Locally preview production build:
+
+```bash
+pnpm run preview
+```
+
+Check out the [deployment documentation](https://nuxt.com/docs/getting-started/deployment) of the Nuxt 3 docs for more information.
